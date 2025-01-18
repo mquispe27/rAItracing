c05dec6b308141c5a989d828beeaa01c4f38f077
Matthew Quispe <mquispe@mit.edu>
2025-01-18 15:53:40 -0500

    Initial Commit

---
+++ rAItracing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rAItracing
+//
+//  Created by Matthew Quispe on 1/18/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
