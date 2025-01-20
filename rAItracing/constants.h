f8383e42e72b25a01265e22f6e27678e060818a2
Matthew Quispe <mquispe@mit.edu>
2025-01-19 21:41:01 -0500

    metals and reflection + some missing files

--- rAItracing/constants.h
+++ rAItracing/constants.h
@@ -9,6 +9,7 @@
 #define CONSTANTS_H
 
 #include <cmath>
+#include <random>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -30,11 +31,22 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
-// Common Headers
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
 
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
+}
+
+// Common Headers
 #include "color.h"
+#include "vec3.h"
 #include "interval.h"
 #include "ray.h"
-#include "vec3.h"
+
 
 #endif
