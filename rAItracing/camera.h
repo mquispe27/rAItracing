4d1383dd0279d40bb081a4b5abe997717deff36f
Matthew Quispe <mquispe@mit.edu>
2025-01-19 21:43:30 -0500

    missing files

---
+++ rAItracing/camera.h
@@ -0,0 +1,148 @@
+//
+//  camera.h
+//  rAItracing
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#include "hittable.h"
+#include "material.h"
+
+struct RGB{
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+};
+
+
+class camera {
+  public:
+    double aspect_ratio = 1.0;  // Ratio of image width over height
+    int    image_width  = 100;  // Rendered image width in pixel count
+    int    samples_per_pixel = 10;   // Count of random samples for each pixel
+    int    max_depth         = 10;   // Maximum number of ray bounces into scene
+
+    void render(const hittable& world) {
+        initialize();
+
+        std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+        
+        struct RGB data[image_height][image_width];
+
+        for (int j = 0; j < image_height; j++) {
+            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+            for (int i = 0; i < image_width; i++) {
+//                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+//                auto ray_direction = pixel_center - center;
+//                ray r(center, ray_direction);
+//
+//                color pixel_color = ray_color(r, world);
+                
+                color pixel_color(0,0,0);
+                for (int sample = 0; sample < samples_per_pixel; sample++) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, max_depth, world);
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
+                
+                // Apply a linear to gamma transform for gamma 2
+                auto r = linear_to_gamma(pixel_samples_scale*pixel_color.x());
+                auto g = linear_to_gamma(pixel_samples_scale*pixel_color.y());
+                auto b = linear_to_gamma(pixel_samples_scale*pixel_color.z());
+                
+                static const interval intensity(0.000, 0.999);
+                data[j][i].R = static_cast<unsigned char>(256 * intensity.clamp(r));
+                data[j][i].G = static_cast<unsigned char>(256 * intensity.clamp(g));
+                data[j][i].B = static_cast<unsigned char>(256 * intensity.clamp(b));
+    
+    
+                write_color(std::cout, pixel_color);
+            }
+        }
+        
+        stbi_write_jpg("/Users/mquispe/Documents/IAP 2025/cpp-projects/rAItracing/rAItracing/metal_image.jpg", image_width, image_height, 3, data, 100);
+        std::clog << "\rDone.                 \n";
+    }
+
+  private:
+    int    image_height;   // Rendered image height
+    double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
+    point3 center;         // Camera center
+    point3 pixel00_loc;    // Location of pixel 0, 0
+    vec3   pixel_delta_u;  // Offset to pixel to the right
+    vec3   pixel_delta_v;  // Offset to pixel below
+
+    void initialize() {
+        image_height = int(image_width / aspect_ratio);
+        image_height = (image_height < 1) ? 1 : image_height;
+        
+        pixel_samples_scale = 1.0 / samples_per_pixel;
+
+        center = point3(0, 0, 0);
+
+        // Determine viewport dimensions.
+        auto focal_length = 1.0;
+        auto viewport_height = 2.0;
+        auto viewport_width = viewport_height * (double(image_width)/image_height);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        auto viewport_u = vec3(viewport_width, 0, 0);
+        auto viewport_v = vec3(0, -viewport_height, 0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        pixel_delta_u = viewport_u / image_width;
+        pixel_delta_v = viewport_v / image_height;
+
+        // Calculate the location of the upper left pixel.
+        auto viewport_upper_left =
+            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    }
+    
+    ray get_ray(int i, int j) const {
+       // Construct a camera ray originating from the origin and directed at randomly sampled
+       // point around the pixel location i, j.
+
+       auto offset = sample_square();
+       auto pixel_sample = pixel00_loc
+                         + ((i + offset.x()) * pixel_delta_u)
+                         + ((j + offset.y()) * pixel_delta_v);
+
+       auto ray_origin = center;
+       auto ray_direction = pixel_sample - ray_origin;
+
+       return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() const {
+       // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+       return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+    
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        // If we've exceeded the ray bounce limit, no more light is gathered.
+        if (depth <= 0)
+            return color(0,0,0);
+
+        hit_record rec;
+
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            ray scattered;
+           color attenuation;
+           if (rec.mat->scatter(r, rec, attenuation, scattered))
+               return attenuation * ray_color(scattered, depth-1, world);
+           return color(0,0,0);
+        }
+
+        vec3 unit_direction = unit_vector(r.direction());
+        auto a = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    }
+};
+
+#endif
+
